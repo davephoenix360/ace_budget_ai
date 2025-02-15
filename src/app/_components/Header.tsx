ba7f97d22eef12c2609a7a25438fed93275462ff
Diepreye D. Charles-Daniel <davediepreye05@gmail.com>
2025-02-15 15:40:04 -0700

    Merge complete

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,30 +2,32 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-// import { useUser, UserButton } from "@clerk/nextjs";
+import { useUser, UserButton, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 function Header() {
-//   const { user, isSignedIn } = useUser();
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
         <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
         <span className="text-blue-800  font-bold text-xl">FinanSmart</span>
       </div>
-      {/* {isSignedIn ? (
-        <UserButton />
-      ) : ( */}
-        <div className="flex gap-3  items-center">
+      {isSignedIn ? (
+        <div className="flex gap-3 items-center">
           <Link href={"/dashboard"}>
             <Button variant="outline" className="rounded-full">
               Dashboard
             </Button>
           </Link>
-          <Link href={"/sign-in"}>
-            <Button className="rounded-full">Get Started</Button>
-          </Link>
+          <UserButton />
         </div>
-      {/* )} */}
+      ) : (
+        <div className="flex gap-3  items-center">
+          <SignInButton>
+            <Button className="rounded-full">Get Started</Button>
+          </SignInButton>
+        </div>
+      )}
     </div>
   );
 }

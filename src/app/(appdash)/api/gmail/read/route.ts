4d91dda744c39e2c29f308d18245b612748ef462
Diepreye D. Charles-Daniel <davediepreye05@gmail.com>
2025-02-15 21:33:51 -0700

    Gmail API is working now

---
+++ src/app/(appdash)/api/gmail/read/route.ts
@@ -0,0 +1,40 @@
+// api/gmail/read/route.ts
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+import { fetchGmailToken, getOAuth2Client } from "@/firebase/gmailToken";
+
+export async function GET(request: Request) {
+  try {
+    const oauth2Client = await getOAuth2Client();
+    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+    const response = await gmail.users.messages.list({
+      userId: "me",
+      maxResults: 100,
+    });
+
+    const messages = response.data.messages || [];
+    const messageData = await Promise.all(
+      messages.map(async (message) => {
+        const messageResponse = await gmail.users.messages.get({
+          userId: "me",
+          id: message.id || "",
+        });
+        return messageResponse.data;
+      })
+    );
+
+    // Only return the snippet and body of each message
+    const finalData = messageData.map((message) => ({
+        snippet: message.snippet,
+        body: message.payload?.body?.data,
+        }));
+
+    return NextResponse.json(finalData);
+  } catch (error) {
+    console.error("Error reading messages:", error);
+    return NextResponse.json(
+      { error: "Error reading Gmail messages", details: error },
+      { status: 500 }
+    );
+  }
+}
